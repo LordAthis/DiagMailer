649ef315844f11933d270586ecc0a60707eb9b1f
LordAthis <lordathis@gmail.com>
2026-09-08 02:37:13 +0200

    Update script version from 3.3.1 to 3.3.4

--- SendReport.ps1
+++ SendReport.ps1
@@ -50,7 +50,7 @@ if (-not $isAdmin) {
 # ===========================================================
 
 $ErrorActionPreference = "Stop"
-$script:Version        = "3.3.1"
+$script:Version        = "3.3.4"
 $script:CredStorePath  = "$env:LOCALAPPDATA\DiagMailer\credential.xml"
 $script:ZipPath        = $null
 
