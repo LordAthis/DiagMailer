b8239dcde765bbd1dc25f8b4d1710944b89dab37
LordAthis <lordathis@gmail.com>
2026-09-08 03:00:31 +0200

    Bump version from 3.3.4 to 3.3.5

--- SendReport.ps1
+++ SendReport.ps1
@@ -50,7 +50,7 @@ if (-not $isAdmin) {
 # ===========================================================
 
 $ErrorActionPreference = "Stop"
-$script:Version        = "3.3.4"
+$script:Version        = "3.3.5"
 $script:CredStorePath  = "$env:LOCALAPPDATA\DiagMailer\credential.xml"
 $script:ZipPath        = $null
 
